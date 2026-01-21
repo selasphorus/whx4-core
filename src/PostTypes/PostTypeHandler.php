c8b27f4728212dfe96090fa18cd8e03717783381
atc-mba25 <birdhive@gmail.com>
2026-01-21 10:07:00 -0500

    TS cont.

--- src/PostTypes/PostTypeHandler.php
+++ src/PostTypes/PostTypeHandler.php
@@ -601,6 +601,7 @@ abstract class PostTypeHandler extends BaseHandler
 	{
 		$post = get_post();
 		$postType = get_post_type();
+		error_log( '[PostTypeHandler] postType: ' . $postType . '' );
 		if ( $postType == "whx4_event" ) { $postType = "event"; } // Tmp WIP
 	
 		if ( ! is_singular( $postType ) || ! in_the_loop() || ! is_main_query()  || !$post instanceof \WP_Post) {
